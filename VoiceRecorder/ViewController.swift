e59859c12b41fcee91651b908f8cfa24786588e2
Raul Calvo Martin <raul.calvo.martin@citrix.com>
2021-07-08 13:54:25 +0200

    Initial Commit

---
+++ VoiceRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceRecorder
+//
+//  Created by raul_builder on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
